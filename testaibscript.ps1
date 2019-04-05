69cc67a6c3bee80d340c924878c79bdc312fc90c
Connor McCardle <49231782+ConnorGSM@users.noreply.github.com>
2019-04-05 11:45:08 +0100

    Add files via upload

--- testaibscript.ps1
+++ testaibscript.ps1
@@ -4,7 +4,7 @@ echo AIB Works > C:\AIB\aibproof.txt
 #endregion
 
 #region Download Application
-(New-Object System.Net.WebClient).DownloadFile("https://aibmedia.blob.core.windows.net/media/7Zip.exe", "C:\AIB\7Zip.exe")
+(New-Object System.Net.WebClient).DownloadFile("https://aibmedia.blob.core.windows.net/media/7Zip.exe?sp=r&st=2019-04-05T10:41:52Z&se=2020-01-01T19:41:52Z&spr=https&sv=2018-03-28&sig=OvkE7gcfH118wtdZ%2F2kBzLdPSU%2ByHjwiuQgSl1%2BV1Gs%3D&sr=b", "C:\AIB\7Zip.exe")
 #endregion
 
 Start-Sleep -Seconds 120
